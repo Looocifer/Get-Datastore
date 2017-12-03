5d4ba32eca5555e5bb381703b401f0787278f8b4
Prithu Ganguli <ganguli.prithu@hotmail.com>
2017-12-03 22:01:05 +0530

    Added FreePercentage Column

--- Get-Datastore.ps1
+++ Get-Datastore.ps1
@@ -1,12 +1,13 @@
-﻿$gds = Get-Datastore
+$gds = Get-Datastore
 $capture = @()
 foreach ($ds in $gds) {
 $dsview = $ds | Get-View
-$thisDS = "" | Select "DatastoreName","CanonicalName","OverallStatus","TotalSpaceTB","TotalSpaceGB","FreeSpaceGB"
+$thisDS = "" | Select "DatastoreName","CanonicalName","OverallStatus","TotalSpaceTB","TotalSpaceGB","FreeSpaceGB","FreePercentage"
 $thisDS."DatastoreName" = $ds.Name
 $thisDS."CanonicalName" = $dsview.info.vmfs.extent
 $thisDS."OverallStatus" = $dsview.OverallStatus
 $thisDS."TotalSpaceTB" = $ds.CapacityGB/1024
+$thisDS."FreePercentage" = ($ds.FreeSpaceGB/$ds.CapacityGB)*100
 $thisDS."TotalSpaceGB" = $ds.CapacityGB
 $thisDS."FreeSpaceGB" = $ds.FreeSpaceGB
 $capture += $thisDS
